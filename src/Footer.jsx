dba135187cf7e96a8d2c90806ec119eaef9299a2
thanis-26 <thanisannaljenifer@gmail.com>
2026-08-11 19:04:25 -0500

    Refine certificates and footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,4 +1,4 @@
-﻿import React from "react";
+import React from "react";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
-function Footer() { return <footer className="site-footer"><div className="footer-left"><p>Website by Jen: Salesforce Administrator, Business Analyst & Platform Developer</p><p>© 2026 Jen.</p></div><div className="footer-right"><a href="https://github.com/thanis-26" target="_blank" rel="noopener noreferrer"><FaGithub /></a><a href="https://www.linkedin.com/in/thanis-annaljenifer-kennady/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a></div></footer>; }
+function Footer() { return <footer className="site-footer"><div className="footer-left"><p>Website by Jen</p></div><div className="footer-right"><a href="https://github.com/thanis-26" target="_blank" rel="noopener noreferrer"><FaGithub /></a><a href="https://www.linkedin.com/in/thanis-annal-jenifer-kennady-859283174/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a></div></footer>; }
 export default Footer;
